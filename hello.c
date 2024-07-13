6538eff65e4bbb742f31d9970d38d5914ea5d28d
RiyaBangad <riyabangad84@gmail.com>
2024-07-13 10:27:29 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,5 +3,5 @@ int main()
 {
     printf("\n riya bangad");
     printf("\n I am studying about Git and Github");
-    printf("\n its a interesting to learn new skills");
+    printf("\n its interesting to learn new skills");
 }
