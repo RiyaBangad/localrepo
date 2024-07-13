6adcd97ded9ae470c137ea5ca63099946ea77a57
RiyaBangad <riyabangad84@gmail.com>
2024-07-13 10:42:32 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -4,6 +4,7 @@ int main()
     printf("\n riya bangad");
     printf("\n I am studying about Git and Github");
     printf("\n its interesting to learn new skills");
-    printf("\n I am first engineering student); 
+    printf("\n I am first engineering student");
+    printf("\n i am very keen to learn and explore");
 }
 
